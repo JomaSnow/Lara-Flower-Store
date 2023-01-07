f6d845f20852ffedf2bb92aec1a6a8870362089d
JomaSnow <jm.duda@uol.com.br>
2023-01-07 00:17:36 -0300

    :heavy_plus_sign: Adds Sanctum dependency. Adds protected Routes.

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Método simples que cria todas as rotas básicas para um CRUD. Como vamos proteger algumas e outras não, vamos declarar cada uma indiviudalmente.
+// Route::resource('products', ProductController::class);
 
-Route::resource('products', ProductController::class);
+// Public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
