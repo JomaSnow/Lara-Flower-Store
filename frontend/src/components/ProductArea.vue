cde64633a7a42c90737cf3742d49634957b8a9c8
JomaSnow <jm.duda@uol.com.br>
2023-01-15 06:30:27 -0300

    :construction: WIP: Product Card.

--- frontend/src/components/ProductArea.vue
+++ frontend/src/components/ProductArea.vue
@@ -1,8 +1,53 @@
 <template>
   <section>
-    <ProductCard title="sad" />
-    <ProductCard title="sad" />
-    <ProductCard title="sad" />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/491/1080/1080.jpg?hmac=sbOs1W6aVQp2zOfsBd83HSaOOcoecGP4jvfWK_Yx7uI"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/335/1080/1080.jpg?hmac=CnZ6yjqZf07B84U_0-HQzMIar3qd454CYl3VWMcbbMo"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/427/1080/1080.jpg?hmac=PLOPDGWH-r46QJfWdvySql51VA507HztungCXCbu0xs"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/126/1080/1080.jpg?hmac=8M0vN_90Z0fwoiXrIA8JD0lJ4B6hoZWH--RgeqOm9zo"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/491/1080/1080.jpg?hmac=sbOs1W6aVQp2zOfsBd83HSaOOcoecGP4jvfWK_Yx7uI"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/335/1080/1080.jpg?hmac=CnZ6yjqZf07B84U_0-HQzMIar3qd454CYl3VWMcbbMo"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/427/1080/1080.jpg?hmac=PLOPDGWH-r46QJfWdvySql51VA507HztungCXCbu0xs"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/126/1080/1080.jpg?hmac=8M0vN_90Z0fwoiXrIA8JD0lJ4B6hoZWH--RgeqOm9zo"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/491/1080/1080.jpg?hmac=sbOs1W6aVQp2zOfsBd83HSaOOcoecGP4jvfWK_Yx7uI"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/335/1080/1080.jpg?hmac=CnZ6yjqZf07B84U_0-HQzMIar3qd454CYl3VWMcbbMo"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/427/1080/1080.jpg?hmac=PLOPDGWH-r46QJfWdvySql51VA507HztungCXCbu0xs"
+    />
+    <ProductCard
+      title="sad"
+      imgPath="https://i.picsum.photos/id/126/1080/1080.jpg?hmac=8M0vN_90Z0fwoiXrIA8JD0lJ4B6hoZWH--RgeqOm9zo"
+    />
   </section>
 </template>
 
@@ -17,10 +62,13 @@ export default {
 
 <style scoped lang="scss">
 section {
+  position: relative;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-evenly;
   width: 100%;
+  padding: 200px 0 0;
+  overflow: hidden;
 }
 </style>
--- frontend/src/components/ProductCard.vue
+++ frontend/src/components/ProductCard.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
-    class="card"
-    @mousemove.prevent="moveCard"
-    @mouseout.prevent="resetCard"
-  ></div>
+  <img class="imgBg" :src="this.imgPath" :alt="this.title" :style="cssVars" />
+  <div ref="el" class="card" @mousemove="moveCard" @mouseout="resetCard">
+    <img class="card-img" :src="this.imgPath" :alt="this.title" />
+    <span>{{ this.title }}</span>
+  </div>
 </template>
 
 <script>
@@ -16,41 +16,76 @@ export default {
     },
     imgPath: String,
   },
+  data() {
+    return { opacity: 0 };
+  },
+  computed: {
+    cssVars() {
+      return {
+        "--opacity": this.opacity,
+      };
+    },
+  },
   methods: {
     moveCard(event) {
-      const position = event.target.getBoundingClientRect();
+      this.opacity = 1;
+
+      const position = this.$refs.el.getBoundingClientRect();
       const x =
         event.pageX - window.scrollX - position.left - position.width / 2;
       const y =
         event.pageY - window.scrollY - position.top - position.height / 2;
 
-      event.target.style.transform =
-        "translate(" + x * 0.1 + "px, " + y * 0.1 + "px)";
+      this.$refs.el.style.transform =
+        "translate(" + x * 0.2 + "px, " + y * 0.3 + "px)";
+      this.$refs.el.style.transition = "transform 0s ease-out";
     },
 
-    resetCard(event) {
-      event.target.style.transform = "translate(0px, 0px)";
+    resetCard() {
+      this.opacity = 0;
+
+      this.$refs.el.style.transform = "translate(0px, 0px)";
+      this.$refs.el.style.transition = "transform 0.15s ease-out";
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
+.imgBg {
+  position: fixed;
+  z-index: -1;
+  opacity: var(--opacity);
+  transition: all 0.8s linear;
+  width: 125%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  right: 0;
+  filter: blur(8px) saturate(0.4);
+}
 .card {
+  border: 1px solid red;
   position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  background-color: saddlebrown;
   height: 600px;
   width: 450px;
   margin: 20px 50px 80px;
   transform: translate(0px, 0px);
 
   transition-property: transform;
-  transition-duration: 0.15s;
+  transition-duration: 0.1s;
   transition-timing-function: linear;
-  /* transition-timing-function: ease-out; */
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+.card-img {
+  height: 80%;
+  width: 100%;
 }
 </style>
